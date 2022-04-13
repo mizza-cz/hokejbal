c1672dfba1ba8411105491efff87829e126312c3
mizza-cz <mizza.cz666@gmail.com>
2022-04-13 08:43:37 +0200

    calendar add

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,8 +1,8 @@
 //=include ../../node_modules/vanilla-lazyload/dist/lazyload.js
-//=include ../../node_modules/carusel/dist/carusel.js
-//=include ../../node_modules/smooth-scroll/dist/smooth-scroll.polyfills.min.js
 
+//=include ../../node_modules/smooth-scroll/dist/smooth-scroll.polyfills.min.js
 document.addEventListener("DOMContentLoaded", function() {
 //=include ../components/**/*.js
+
 //=include custom/*.js
 });
