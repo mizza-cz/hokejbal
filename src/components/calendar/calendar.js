d9f621286b7702972871c60386f016611c15ad2e
mizza-cz <mizza.cz666@gmail.com>
2022-04-22 09:43:37 +0200

    add typo

--- src/components/calendar/calendar.js
+++ src/components/calendar/calendar.js
@@ -2,28 +2,20 @@
  
  (function () {    
      'use strict';
-     // ------------------------------------------------------- //
-     // Calendar
-     // ------------------------------------------------------ //
     jQuery(function() {
-       // page is ready
        jQuery('#calendar').fullCalendar({
           locale: 'cz',
           defaultView: 'month',
-          // event dragging & resizing
           editable: true,
-          // header
           header: {
              left: 'prev',
              center: 'title',
              right: 'next'
           },
           monthNames: ['LEDEN', 'ÚNOR', 'BŘEZEN', 'DUBEN', 'KVĚTEN', 'ČERVEN', 'ČERVENEC', 'SRPEN', 'ZÁŘÍ', 'ŘÍJEN', 'LISTOPAD', 'PROSINEC'],
-        // 月略称
         monthNamesShort: ['LEDEN', 'ÚNOR', 'BŘEZEN', 'DUBEN', 'KVĚTEN', 'ČERVEN', 'ČERVENEC', 'SRPEN', 'ZÁŘÍ', 'ŘÍJEN', 'LISTOPAD', 'PROSINEC'],
         dayNames: ['NEDĚLE','PONDĚLÍ', 'ÚTERÝ', 'STŘEDA', 'ČTVRTEK', 'PÁTEK', 'SOBOTA'],
         dayNamesShort: ['NE', 'PO', 'ÚT', 'ST', 'ČT', 'PÁ', 'SO'],
-       
           events: [
              {
                 id: '1',
@@ -34,7 +26,6 @@
                 categorie: 'a-tym'
              },
              {
-                
                id: '2',
                start: '2022-04-08',
                title: '18:00',
@@ -42,26 +33,15 @@
                url: '/akce', 
                categorie: 'mladez'
              },
-            
           ],
-          
           eventRender: function(event, element) {
              if(event.icon){
-               //  element.find(".fc-title").prepend('<img src="images/content/'+event.icon+'.png" />');
                element.find(".fc-title").prepend('<img src="'+event.icon+'" />');
              }
              if(event.icon){
-               //  element.find(".fc-title").prepend('<img src="images/content/'+event.icon+'.png" />');
                element.find(".fc-title").addClass(''+event.categorie+'');
              }
-             
             },
-           
-            
        })
-      //  $('.fc-day-number').on('click', function () {
-      //       console.log(this)
-      //  });
     });
-   
  })(jQuery);
